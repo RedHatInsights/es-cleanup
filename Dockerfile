4302fcd71930f735e7f479df3bdba3f56f76c5fc
Blake Holifield <bholifie@redhat.com>
2020-05-28 12:22:05 -0500

    update to delete only a two week old index, run daily

--- Dockerfile
+++ Dockerfile
@@ -3,4 +3,4 @@ COPY . /app
 WORKDIR /app
 RUN pip install -r requirements.txt 
 RUN chmod +x delete-indices.py
-CMD ["./delete-indices.py", "hostname.com.gov", "2020.05.10"]
+CMD ["./delete-indices.py"]
--- delete-indices.py
+++ delete-indices.py
@@ -1,13 +1,15 @@
 #!/usr/bin/env python3
 
 """
-Deletes indexes in ElasticSearch that are older than one week.
+Deletes indexes in ElasticSearch that are two weeks.
 Example invocation:
 
-./delete_indexes.py my-cluster.us-east-1.es.amazonaws.com 2019.07.01
+./delete_indexes.py 
 
-Which will delete all the indexes starting with cwl-2019.07.01 until the index
-that is one week old
+Which will delete the index that is two weeks old from the day of the run.
+If the current date is 2020.05.28, the index cwl-2020.05.14 will be deleted
+
+This script is intended to run each morning as an oc cronjob. 
 
 This assumes there is NO authentication on your cluster!
 """
@@ -17,30 +19,14 @@ import sys
 import requests
 import os
 
-"""
-this dude needs a couple things:
-1. Either a passed in date of the current cron job (2020.05.25) or just grab the current date from the api
-2. A hostname to be pointed at (could split to 2 jobs or just do both with one)
-
-"""
-
-if len(sys.argv) < 3:
-    print("provide server hostname and index start date YYYY.mm.dd")
-    sys.exit(0)
-
 hostname = os.environ['ES_HOSTNAME']
-print(f"going to {hostname}")
 
 today = date.today()
 two_weeks_ago = today + timedelta(days=-14)
 
-# index_date = date(**dict(zip(["year", "month", "day"], [int(i) for i in start_date.split(".")])))
-
 index_name = "cwl-%s" % two_weeks_ago.strftime("%Y.%m.%d")
-# r = requests.delete(f"https://{hostname}/{index_name}")
-# if r.status_code == 200:
-print(f"Deleted index {index_name}, but not really ;0")
-# else:
-#     raise ValueError("Bad response code: %d" % r.status_code)
-
-# index_date = index_date + timedelta(days=1)
+r = requests.delete(f"https://{hostname}/{index_name}")
+if r.status_code == 200:
+    print(f"Deleted index {index_name}")
+else:
+    raise ValueError("Bad response code: %d" % r.status_code)
