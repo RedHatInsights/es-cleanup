dfb470414c8c29d254a01c4ebd608f694393af4d
bholifie <bholifie@redhat.com>
2020-10-22 15:40:55 -0500

    Also works on SRE indexes

--- delete-indices.py
+++ delete-indices.py
@@ -4,12 +4,13 @@
 Deletes indexes in ElasticSearch that are two weeks.
 Example invocation:
 
-./delete_indexes.py 
+./delete_indexes.py
 
 Which will delete the index that is two weeks old from the day of the run.
-If the current date is 2020.05.28, the index cwl-2020.05.14 will be deleted
+If the current date is 2020.05.28, the index cwl-2020.05.14 or cwl-$loggroup-* will be deleted based
+on env
 
-This script is intended to run each morning as an oc cronjob. 
+This script is intended to run each morning as an oc cronjob.
 
 This assumes there is NO authentication on your cluster!
 """
@@ -20,23 +21,33 @@ import requests
 import os
 
 hostname = os.environ['ES_HOSTNAME']
-index_delted = "index_not_found_exception"
+sre_managed = os.environ['SRE_MANAGED']
+index_deleted = "index_not_found_exception"
 
 today = date.today()
 # Note: 05.19 is the last day with data, so 06.02 is our first available window
 two_weeks_ago = today + timedelta(days=-14)
 
-index_name = "cwl-%s" % two_weeks_ago.strftime("%Y.%m.%d")
+index_name = ""
+date_string = two_weeks_ago.strftime("%Y.%m.%d")
+
+# Loaded in from the deploy template. True is a string and not a boolean value
+if sre_managed == "True":
+    # App sre managed indexes have the log group name in them; v3 clusters do not
+    index_name = f"cwl-*-{date_string}"
+else:
+    index_name = f"cwl-{date_string}"
+
 print(f"Processing delete for https://{hostname}/{index_name}")
 r = requests.delete(f"https://{hostname}/{index_name}")
 
 response_body = r.json()
 if r.status_code == 200:
-    print(f"Deleted index {index_name}")
-elif r.status_code == 404: 
+    print(f"Deleted index for {date_string}")
+elif r.status_code == 404:
     root_cause = response_body["error"]["root_cause"][0]
-    if root_cause["type"] == index_delted:
-        print(f"Index {index_name} is already deleted")
+    if root_cause["type"] == index_deleted:
+        print("Index is already deleted")
 else:
-    raise ValueError("Unexpected response code: {}".format(r.status_code)) 
-    
+    raise ValueError("Unexpected response code: {}".format(r.status_code))
+
